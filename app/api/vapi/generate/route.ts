930d03e40ec3c3b04fd7376f3d614e7352e82354
krishnakumar51 <godkrishnasskal@gmail.com>
2025-07-15 21:53:04 +0530

    comm\

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -9,7 +9,9 @@ export async function GET(){
 
 
 export async function POST(request: Request){
-    const {type, role, level, techstack, amount, userid} = await request.json();
+    const requestBody = await request.json();
+    console.log('Full request body received by API route:', requestBody);
+    const {type, role, level, techstack, amount, userid} = requestBody;
 
     try {
 
@@ -41,7 +43,7 @@ export async function POST(request: Request){
             createdAt: new Date().toISOString(),
         }
 
-        console.log('Interview object before saving to Firebase:', interview);
+
         await db.collection('interviews').add(interview);
 
         return Response.json({ success: true }, { status: 200 });
